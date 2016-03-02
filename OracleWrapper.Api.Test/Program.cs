f37fc1710f7e1327e015a9fc65d7cc3fb8a54bee
iovigi <il.nedelchev@gmail.com>
2016-03-02 17:37:06 +0200

    one line test

--- OracleWrapper.Api.Test/Program.cs
+++ OracleWrapper.Api.Test/Program.cs
@@ -10,8 +10,10 @@
             Database<int, int> database = new Database<int, int>("file.oracle");
 
             database.Add(1, 2);
+            database.Clear();
 
             Console.WriteLine(database.ContainsKey(2));
+            database[1] = 3;
             Console.WriteLine(database[1]);
             Console.WriteLine(database.Count);
         }
